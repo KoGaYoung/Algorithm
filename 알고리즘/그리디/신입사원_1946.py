a40d7bcc985379f5859837eba043d70d5cdd29e1
ko_gayoung <j4723515@gmail.com>
2021-06-16 08:58:59 +0900

    신입사원클리어

--- 알고리즘/그리디/신입사원_1946.py
+++ 알고리즘/그리디/신입사원_1946.py
@@ -1,44 +1,28 @@
-# 2
-# 5
-# 3 2
-# 1 4
-# 4 1
-# 2 3
-# 5 5
-# 7
-# 3 6
-# 7 3
-# 4 2
-# 1 4
-# 5 7
-# 2 5
-# 6 1
-t = int(input())
+import sys
+t = int(sys.stdin.readline())
 for _ in range(0, t):
-    n = int(input())
+    n = int(sys.stdin.readline())
 
-    applicants = []
-    for _ in range(n):
-        applicants.append(list(map(int, input().split())))
+    applicants = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+    applicants.sort(key=lambda x: (x[0]))
 
-    # print(applicants)
-    applicants.sort(key=lambda x: (x[0], x[1]))
-    # print(applicants)
-    flag = [True for x in range(0, n)]
+    answer = 1
+    m = applicants[0][1]
 
-    for i in range(0, n):
-        if flag[i]:
+    for i in range(1, n):
+        if applicants[i][1] < m:
+            answer += 1
             m = applicants[i][1]
-            for j in range(i + 1, n):
-                if m < applicants[j][1]:
-                    flag[j] = False
-        else:
-            pass
 
-    count = 0
-    for i in flag:
-        if i:
-            count += 1
-    print(count)
-    # print(flag)
+    print(answer)
+    # flag = [1 for _ in range(n)]
+    #
+    # for i in range(0, n):
+    #     if flag[i] == 1:
+    #         m = applicants[i][1]
+    #         for j in range(i + 1, n):
+    #             if m < applicants[j][1]:
+    #                 flag[j] = 0
+    #
+    # print(sum(flag))
 
