979d1389aeaa5be5f2304835bcfa2ec8604b3c2d
고가영 <4723515@naver.com>
2021-11-02 18:30:54 +0900

    푸시테스트 예쩨

--- 알고리즘/그리디/단어수학_1339.py
+++ 알고리즘/그리디/단어수학_1339.py
@@ -1,18 +1,33 @@
 import sys
 
+from collections import Counter
+
 if __name__ == "__main__":
     ''' 
     문자열 길이순 정렬 후 최빈값을 배열에 저장
     '''
+
     n = int(sys.stdin.readline())
-    word = [sys.stdin.readline() for _ in range(n)]
+    m_len = 0
+    words = {'9': '', '8': '', '7': ''}
+    for _ in range(n):
+        w = sys.stdin.readline()
+        words.append(w)
+        if len(w) > m_len:
+            m_len = len(w)
 
-    word.sort(key=len, reverse=True)
-    w_c = [[] for _ in word[0]]
-
-    for w in word:
+    w_c = [[] for _ in range(0, m_len)]
+    for w in words:
         for i, ch in enumerate(w):
             w_c[i].append(ch)
 
-    from collections import Counter
+    m_num = 9
+    dicn = {''}
+    for ws in w_c:
+        cnt = Counter(ws).most_common()
+        for i,item in enumerate(cnt):
+            dicn[item[i]] = m_num
+            m_num -= 1
+
+    print(dicn)
 
